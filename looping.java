e215174fc7dc46d0b4aa11ae0ec3bdfd9d7f611a
yagnakesana <110729702+yagnakesana@users.noreply.github.com>
2022-08-11 17:28:33 +0530

    Add files via upload

---
+++ looping.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class looping {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=8;i++) {
+			if(i%2==0) {
+				System.out.println("even num" +i);
+			}else
+				System.out.println("odd num" +i);
+			
+			
+		}
+		
+
+	}
+
+}
---
+++ oddOrEven.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class oddOrEven {
+
+	public static void main(String[] args) {
+		
+		int x=15;
+		int y=5;
+		
+		if(x%2==0 && y%2==0) {
+			System.out.println("it is odd");
+		}else {
+			System.out.println("it is even");
+		}
+	
+ 
+	}
+
+}

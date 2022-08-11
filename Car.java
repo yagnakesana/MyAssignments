f7722444bf26ee79aa6472d1448e97d5c02300c4
yagnakesana <110729702+yagnakesana@users.noreply.github.com>
2022-08-11 17:24:49 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Car {
+	public String applyBreak() {
+		String Applybreak = "AppliedBreak";
+		System.out.println(Applybreak);
+		return Applybreak;
+			}
+	public String soundHorn() {
+		String SoundHorn = "soundHorn";
+		System.out.println(SoundHorn);
+		return SoundHorn;
+			}
+}
---
+++ bike.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class bike {
+	String applyBreak;
+	String SoundHorn;
+	
+	public static void main(String[] args) {
+		Car obj=new Car();
+		obj.applyBreak();
+		obj.soundHorn();
+		
+		bike obj1=new bike();
+		obj1.applyBreak();
+		obj1.SoundHorn();
+		
+		
+				
+	}
+
+	private void SoundHorn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void applyBreak() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	}
+
+

ab5735fe80909e4455de7bd2d4bceaa086d8ab56
yagnakesana <110729702+yagnakesana@users.noreply.github.com>
2022-08-14 08:27:57 +0530

    Add files via upload

---
+++ duplicateLead.java
@@ -0,0 +1,64 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class duplicateLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+        ChromeDriver driver=new ChromeDriver();
+        
+        driver.get("http://leaftaps.com/opentaps/control/login");
+        
+        driver.manage().window().maximize();
+        
+        driver.findElement(By.id("username")).sendKeys("demosalesManager");
+        
+        driver.findElement(By.id("password")).sendKeys("crmsfa");
+        
+        driver.findElement(By.className("decorativeSubmit")).click();
+        
+        driver.findElement(By.linkText("CRM/SFA")).click();
+        
+        driver.findElement(By.linkText("Leads")).click();
+        
+        driver.findElement(By.linkText("Create Lead")).click();
+        
+        driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Test Leaf");
+        
+        driver.findElement(By.id("createLeadForm_firstName")).sendKeys("yagna");
+        
+        driver.findElement(By.id("createLeadForm_lastName")).sendKeys("k");
+        
+        driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("kesana");
+        
+        driver.findElement(By.name("departmentName")).sendKeys("Testing");
+        
+        driver.findElement(By.name("description")).sendKeys("Selenium Java");
+        
+        driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("kesanayagna@gmail.com");
+        
+        driver.findElement(By.className("smallSubmit")).click();
+        
+        String title = driver.getTitle();
+        System.out.println(title);
+        
+        
+        driver.findElement(By.linkText("Duplicate Lead")).click();
+        driver.findElement(By.id("createLeadForm_companyName")).clear();
+        driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TCS");
+        driver.findElement(By.id("createLeadForm_firstName")).clear();
+        driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Tina");
+        driver.findElement(By.className("smallSubmit")).click();
+        String title1 = driver.getTitle();
+        System.out.println(title1);
+
+	}
+
+}
